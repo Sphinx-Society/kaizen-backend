d9b1971c5b4a9a07cd0baa394ebf418a9b1e75db
AryRosvall <ary.rosvall@gmail.com>
2020-05-01 23:56:12 -0500

    SSKB-8 Added tests MongoDB connection

--- config/index.js
+++ config/index.js
@@ -11,5 +11,6 @@ module.exports = {
     user: process.env.MONGO_USER,
     password: process.env.MONGO_PASS,
     database: process.env.MONGO_DB,
+    mongo_uri: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
   },
 };
