874041ee30f63f464c4b004e4ffb180cfca9b6a9
AryRosvall <ary.rosvall@gmail.com>
2020-05-01 20:12:09 -0500

    SSKB-8 Added MongoDB connection

--- config/index.js
+++ config/index.js
@@ -4,6 +4,12 @@ dotenv.config();
 
 module.exports = {
   server: {
-    port: process.env.PORT
-  }
-}
+    port: process.env.PORT,
+  },
+  mongo: {
+    host: process.env.MONGO_HOST,
+    user: process.env.MONGO_USER,
+    password: process.env.MONGO_PASS,
+    database: process.env.MONGO_DB,
+  },
+};
